75de913b9bf07ec196ba7798c1b5c3a97df465f4
Tiago Alves <tiagoalvesdev@gmail.com>
2021-12-02 17:13:44 -0300

    Ajuste na rota

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,3 @@ Route::get('/jogos', [JogosController::class, 'index'])->name('jogos.index');
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
